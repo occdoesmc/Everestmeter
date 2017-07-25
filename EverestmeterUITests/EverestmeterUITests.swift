fd78f34cde265873757dbad847607c24d05a89d2
Orlando Castiglioni <occ.github@hairesis.com>
2017-07-25 13:05:55 +0200

    Add initial UI tests

--- EverestmeterUITests/EverestmeterUITests.swift
+++ EverestmeterUITests/EverestmeterUITests.swift
@@ -1,9 +1,24 @@
 import XCTest
 
 final class EverestmeterUITests: XCTestCase {
+    fileprivate let app = XCUIApplication()
+
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        XCUIApplication().launch()
+    }
+}
+
+extension EverestmeterUITests {
+    func testAltitude() {
+        app.launchEnvironment = ["SIMULATED_PRESSURE": "90"]
+        app.launch()
+        XCTAssertTrue(app.staticTexts["1014m"].isHittable)
+    }
+
+    func testOtherAltitude() {
+        app.launchEnvironment = ["SIMULATED_PRESSURE": "100"]
+        app.launch()
+        XCTAssertTrue(app.staticTexts["112m"].isHittable)
     }
 }
