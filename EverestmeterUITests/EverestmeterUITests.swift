c4c015bf1eaedcdda275d020b691775e122ba59d
Orlando Castiglioni <occ.github@hairesis.com>
2017-07-27 15:31:12 +0200

    Add more UI tests

--- EverestmeterUITests/EverestmeterUITests.swift
+++ EverestmeterUITests/EverestmeterUITests.swift
@@ -11,14 +11,42 @@ final class EverestmeterUITests: XCTestCase {
 
 extension EverestmeterUITests {
     func testAltitude() {
-        app.launchEnvironment = ["SIMULATED_PRESSURE": "90"]
-        app.launch()
+        app.launch(withSimulatedPressure: 90)
         XCTAssertTrue(app.staticTexts["1014m"].isHittable)
     }
 
     func testOtherAltitude() {
-        app.launchEnvironment = ["SIMULATED_PRESSURE": "100"]
-        app.launch()
+        app.launch(withSimulatedPressure: 100)
         XCTAssertTrue(app.staticTexts["112m"].isHittable)
+    }
+
+    func testSingleStaticText() {
+        app.launch(withSimulatedPressure: 100)
+        XCTAssertEqual(app.staticTexts.count, 1)
+    }
+
+    func testStaticTextVerticalOffset() {
+        app.launch(withSimulatedPressure: 100)
+        let lower = app.staticTexts.element(boundBy: 0).frame.midY
+        app.launch(withSimulatedPressure: 90)
+        let higher = app.staticTexts.element(boundBy: 0).frame.midY
+        XCTAssertGreaterThan(lower, higher)
+    }
+
+    func testError() {
+        app.launch()
+        XCTAssertTrue(app.staticTexts["Barometer Not Available"].isHittable)
+    }
+
+    func testErrorOnlyTextPresent() {
+        app.launch()
+        XCTAssertEqual(app.staticTexts.count, 1)
+    }
+}
+
+private extension XCUIApplication {
+    func launch(withSimulatedPressure simulatedPressure: Int) {
+        launchEnvironment = ["SIMULATED_PRESSURE": String(simulatedPressure)]
+        launch()
     }
 }
