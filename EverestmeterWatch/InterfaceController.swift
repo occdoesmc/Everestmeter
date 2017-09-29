fa893168243abfc0336bc99f52b4ef20e61d604e
Orlando Castiglioni <occ.github@hairesis.com>
2017-09-29 18:11:41 +0200

    Add empty watchOS app

---
+++ EverestmeterWatch/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  EverestmeterWatch Extension
+//
+//  Created by Orlando Cossia Castiglioni on 29.09.17.
+//  Copyright © 2017 Orlando Cossia Castiglioni. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+}
